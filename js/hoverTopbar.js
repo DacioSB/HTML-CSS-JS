19fea7af12cea633dd593b7e69e9ffc967c860af
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2020-05-02 17:20:59 -0400

    treino finalizado

--- js/hoverTopbar.js
+++ js/hoverTopbar.js
@@ -7,6 +7,46 @@ var catItemSubmenu = document.querySelectorAll(".cat-submenu");
 var triangles = document.querySelectorAll(".cat-popover-triangle");
 var contentTransition = document.querySelectorAll(".popover3-content");
 
+var tile = document.querySelectorAll(".tile-regular");
+var wish = document.querySelectorAll(".label-wishlist");
+
+tile[0].addEventListener("mouseenter", function () {
+    wish[0].classList.add("visivel4");
+});
+tile[0].addEventListener("mouseleave", function () {
+    wish[0].classList.remove("visivel4");
+});
+tile[1].addEventListener("mouseenter", function () {
+    wish[1].classList.add("visivel4");
+});
+tile[1].addEventListener("mouseleave", function () {
+    wish[1].classList.remove("visivel4");
+});
+tile[2].addEventListener("mouseenter", function () {
+    wish[2].classList.add("visivel4");
+});
+tile[2].addEventListener("mouseleave", function () {
+    wish[2].classList.remove("visivel4");
+});
+tile[3].addEventListener("mouseenter", function () {
+    wish[3].classList.add("visivel4");
+});
+tile[3].addEventListener("mouseleave", function () {
+    wish[3].classList.remove("visivel4");
+});
+tile[4].addEventListener("mouseenter", function () {
+    wish[4].classList.add("visivel4");
+});
+tile[4].addEventListener("mouseleave", function () {
+    wish[4].classList.remove("visivel4");
+});
+tile[5].addEventListener("mouseenter", function () {
+    wish[5].classList.add("visivel4");
+});
+tile[5].addEventListener("mouseleave", function () {
+    wish[5].classList.remove("visivel4");
+});
+
 catItemSubmenu[0].addEventListener("mouseenter", function () {
     triangles.forEach(triangle => {
         triangle.style.left = "51px";
@@ -105,29 +145,40 @@ setTimeout(() => {
 
 
 login.addEventListener("mouseenter", visivel, false);
-setTimeout(() => {
-    pop.addEventListener("mouseleave", invisivel, false);
-}, 500);
+
+pop.addEventListener("mouseleave", invisivel, false);
+
 function visivel() {
-    setTimeout(() => {
-        pop.classList.add("visivel");
-    }, 200);   
+     pop.classList.add("visivel");
 }
 function invisivel() {
     pop.classList.remove("visivel"); 
 }
 
 sell.addEventListener("mouseenter", visivelSell, false);
-setTimeout(() => {
-    popSell.addEventListener("mouseleave", invisivelSell, false);
-}, 500);
+popSell.addEventListener("mouseleave", invisivelSell, false);
+
 function visivelSell() {
-    setTimeout(() => {
-        popSell.classList.add("visivel2");
-    }, 200);   
+    popSell.classList.add("visivel2");
+  
 }
 function invisivelSell() {
     popSell.classList.remove("visivel2"); 
+}
+
+function wishFunction() {
+    var popup = document.getElementById("wpopup");
+    var popup1 = document.getElementById("wpopup1");
+    var popup2 = document.getElementById("wpopup2");
+    var popup3 = document.getElementById("wpopup3");
+    var popup4 = document.getElementById("wpopup4");
+    var popup5 = document.getElementById("wpopup5");
+    popup.classList.toggle("wish-popup-show");
+    popup1.classList.toggle("wish-popup-show");
+    popup2.classList.toggle("wish-popup-show");
+    popup3.classList.toggle("wish-popup-show");
+    popup4.classList.toggle("wish-popup-show");
+    popup5.classList.toggle("wish-popup-show");
 }
 
 
